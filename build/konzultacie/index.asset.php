8c53b56c392183b0ca2717f31f40ac7dfacb99c6
Andrej Srna <andrej.srna@cognito.cz>
2025-05-12 16:36:07 +0200

    Update slider styles to utilize SCSS imports for Swiper integration; enhance organization and performance. Remove redundant CSS imports and clean up code by eliminating debugging artifacts and unnecessary comments.

--- build/konzultacie/index.asset.php
+++ build/konzultacie/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '37a8c1daf1bd46d1316b');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6d3486f9d2c4df60e839');
