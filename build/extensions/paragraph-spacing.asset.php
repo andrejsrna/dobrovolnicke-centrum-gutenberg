0233c98f0c91202fe857b4438c5610d80d5c17f6
Andrej Srna <andrej.srna@cognito.cz>
2025-06-16 17:34:40 +0200

    Adds disable padding option to Columns block
    Adds a toggle control to the Columns block, allowing users to disable default padding.
    
    This provides more flexibility in controlling the layout and spacing of columns, particularly when finer control over the block's appearance is desired.

--- build/extensions/paragraph-spacing.asset.php
+++ build/extensions/paragraph-spacing.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '8674fe3483fbd0466446');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '8a2602d3e9fd8c11fa37');
--- src/extensions/paragraph-spacing.js
+++ src/extensions/paragraph-spacing.js
@@ -4,11 +4,12 @@ import { addFilter } from '@wordpress/hooks';
 import { createHigherOrderComponent } from '@wordpress/compose';
 import { Fragment } from '@wordpress/element';
 import { InspectorControls } from '@wordpress/block-editor';
-import { PanelBody, __experimentalBoxControl as BoxControl } from '@wordpress/components';
+import { PanelBody, __experimentalBoxControl as BoxControl, ToggleControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 const MARGIN_ATTRIBUTE_NAME = 'customMargin';
 const PADDING_ATTRIBUTE_NAME = 'customPadding';
+const DISABLE_PADDING_ATTRIBUTE_NAME = 'disablePadding';
 
 // Zoznam blokov, pre ktoré chceme pridať odsadenie
 const SUPPORTED_BLOCKS = ['core/paragraph', 'core/columns'];
@@ -17,91 +18,120 @@ const SUPPORTED_BLOCKS = ['core/paragraph', 'core/columns'];
  * Pridá atribúty pre margin a padding do podporovaných blokov.
  */
 function addSpacingAttributes(settings, name) {
-    if (!SUPPORTED_BLOCKS.includes(name)) {
-        return settings;
-    }
+	if (!SUPPORTED_BLOCKS.includes(name)) {
+		return settings;
+	}
 
-    return {
-        ...settings,
-        attributes: {
-            ...settings.attributes,
-            [MARGIN_ATTRIBUTE_NAME]: {
-                type: 'object',
-                default: {},
-            },
-            [PADDING_ATTRIBUTE_NAME]: {
-                type: 'object',
-                default: {},
-            },
-        },
-    };
+	const newAttributes = {
+		...settings.attributes,
+		[MARGIN_ATTRIBUTE_NAME]: {
+			type: 'object',
+			default: {},
+		},
+	};
+
+	if (name === 'core/columns') {
+		newAttributes[DISABLE_PADDING_ATTRIBUTE_NAME] = {
+			type: 'boolean',
+			default: false,
+		};
+	} else {
+		newAttributes[PADDING_ATTRIBUTE_NAME] = {
+			type: 'object',
+			default: {},
+		};
+	}
+
+	return {
+		...settings,
+		attributes: newAttributes,
+	};
 }
 
 /**
  * Komponent vyššieho rádu, ktorý pridáva ovládacie prvky pre margin a padding.
  */
 const withSpacingControl = createHigherOrderComponent((BlockEdit) => {
-    return (props) => {
-        if (!SUPPORTED_BLOCKS.includes(props.name)) {
-            return <BlockEdit {...props} />;
-        }
+	return (props) => {
+		if (!SUPPORTED_BLOCKS.includes(props.name)) {
+			return <BlockEdit {...props} />;
+		}
 
-        const { attributes, setAttributes } = props;
+		const { attributes, setAttributes, name } = props;
 
-        return (
-            <Fragment>
-                <BlockEdit {...props} />
-                <InspectorControls group="dimensions">
-                    <PanelBody title={__('Odsadenie (Margin & Padding)', 'slider')} initialOpen={false}>
-                        <BoxControl
-                            label={__('Margin', 'slider')}
-                            values={attributes[MARGIN_ATTRIBUTE_NAME]}
-                            onChange={(value) => setAttributes({ [MARGIN_ATTRIBUTE_NAME]: value })}
-                            sides={['top', 'right', 'bottom', 'left']}
-                            units={[{ value: 'px', label: 'px' }, { value: '%', label: '%' }, { value: 'em', label: 'em' }, {value: 'rem', label: 'rem'}, {value: 'vw', label: 'vw'}, {value: 'vh', label: 'vh'}]}
-                            allowReset
-                        />
-                        <BoxControl
-                            label={__('Padding', 'slider')}
-                            values={attributes[PADDING_ATTRIBUTE_NAME]}
-                            onChange={(value) => setAttributes({ [PADDING_ATTRIBUTE_NAME]: value })}
-                            sides={['top', 'right', 'bottom', 'left']}
-                            units={[{ value: 'px', label: 'px' }, { value: '%', label: '%' }, { value: 'em', label: 'em' }, {value: 'rem', label: 'rem'}, {value: 'vw', label: 'vw'}, {value: 'vh', label: 'vh'}]}
-                            allowReset
-                        />
-                    </PanelBody>
-                </InspectorControls>
-            </Fragment>
-        );
-    };
+		return (
+			<Fragment>
+				<BlockEdit {...props} />
+				<InspectorControls group="dimensions">
+					<PanelBody title={__('Odsadenie', 'slider')} initialOpen={false}>
+						<BoxControl
+							label={__('Margin', 'slider')}
+							values={attributes[MARGIN_ATTRIBUTE_NAME]}
+							onChange={(value) => setAttributes({ [MARGIN_ATTRIBUTE_NAME]: value })}
+							sides={['top', 'right', 'bottom', 'left']}
+							units={[{ value: 'px', label: 'px' }, { value: '%', label: '%' }, { value: 'em', label: 'em' }, {value: 'rem', label: 'rem'}, {value: 'vw', label: 'vw'}, {value: 'vh', label: 'vh'}]}
+							allowReset
+						/>
+						{name === 'core/columns' ? (
+							<ToggleControl
+								label={__('Vypnúť paddingy', 'slider')}
+								checked={!!attributes[DISABLE_PADDING_ATTRIBUTE_NAME]}
+								onChange={(value) => setAttributes({ [DISABLE_PADDING_ATTRIBUTE_NAME]: value })}
+								help={
+									attributes[DISABLE_PADDING_ATTRIBUTE_NAME]
+										? __('Vlastné paddingy sú vypnuté (nastavené na 0).', 'slider')
+										: __('Zachovať pôvodné paddingy bloku.', 'slider')
+								}
+							/>
+						) : (
+							<BoxControl
+								label={__('Padding', 'slider')}
+								values={attributes[PADDING_ATTRIBUTE_NAME]}
+								onChange={(value) => setAttributes({ [PADDING_ATTRIBUTE_NAME]: value })}
+								sides={['top', 'right', 'bottom', 'left']}
+								units={[{ value: 'px', label: 'px' }, { value: '%', label: '%' }, { value: 'em', label: 'em' }, {value: 'rem', label: 'rem'}, {value: 'vw', label: 'vw'}, {value: 'vh', label: 'vh'}]}
+								allowReset
+							/>
+						)}
+					</PanelBody>
+				</InspectorControls>
+			</Fragment>
+		);
+	};
 }, 'withSpacingControl');
 
 /**
  * Aplikuje štýly pre margin a padding na wrapper element bloku pri ukladaní.
  */
 function applySpacingStylesToSave(extraProps, blockType, attributes) {
-    if (!SUPPORTED_BLOCKS.includes(blockType.name)) {
-        return extraProps;
-    }
+	if (!SUPPORTED_BLOCKS.includes(blockType.name)) {
+		return extraProps;
+	}
 
-    const margin = attributes[MARGIN_ATTRIBUTE_NAME];
-    const padding = attributes[PADDING_ATTRIBUTE_NAME];
-    const newStyles = { ...extraProps.style };
+	const margin = attributes[MARGIN_ATTRIBUTE_NAME];
+	const padding = attributes[PADDING_ATTRIBUTE_NAME];
+	const newStyles = { ...extraProps.style };
 
-    if (margin) {
-        if (margin.top) newStyles.marginTop = margin.top;
-        if (margin.right) newStyles.marginRight = margin.right;
-        if (margin.bottom) newStyles.marginBottom = margin.bottom;
-        if (margin.left) newStyles.marginLeft = margin.left;
-    }
-    if (padding) {
-        if (padding.top) newStyles.paddingTop = padding.top;
-        if (padding.right) newStyles.paddingRight = padding.right;
-        if (padding.bottom) newStyles.paddingBottom = padding.bottom;
-        if (padding.left) newStyles.paddingLeft = padding.left;
-    }
+	if (margin) {
+		if (margin.top) newStyles.marginTop = margin.top;
+		if (margin.right) newStyles.marginRight = margin.right;
+		if (margin.bottom) newStyles.marginBottom = margin.bottom;
+		if (margin.left) newStyles.marginLeft = margin.left;
+	}
 
-    return { ...extraProps, style: newStyles };
+	if (blockType.name === 'core/columns' && attributes[DISABLE_PADDING_ATTRIBUTE_NAME]) {
+		newStyles.paddingTop = '0px';
+		newStyles.paddingRight = '0px';
+		newStyles.paddingBottom = '0px';
+		newStyles.paddingLeft = '0px';
+	} else if (padding) {
+		if (padding.top) newStyles.paddingTop = padding.top;
+		if (padding.right) newStyles.paddingRight = padding.right;
+		if (padding.bottom) newStyles.paddingBottom = padding.bottom;
+		if (padding.left) newStyles.paddingLeft = padding.left;
+	}
+
+	return { ...extraProps, style: newStyles };
 }
 
 addFilter('blocks.registerBlockType', 'slider/add-spacing-attributes', addSpacingAttributes);
