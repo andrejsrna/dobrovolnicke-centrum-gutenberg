8762a706abefb6e33a9948be0f4aaaefb3a7fb76
Andrej Srna <andrej.srna@cognito.cz>
2025-06-16 17:41:41 +0200

    Refactors spacing controls for column blocks.
    Improves spacing controls for column blocks by introducing a toggle to disable all margins and paddings at once.
    
    This change simplifies the user interface and provides more intuitive control over spacing. It also adjusts the logic to conditionally render margin and padding controls based on the toggle state and block type.

--- build/extensions/paragraph-spacing.asset.php
+++ build/extensions/paragraph-spacing.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '444fcc952bd0b75a79d6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '166c0b9f95bb6af48b8f');
--- src/extensions/paragraph-spacing.js
+++ src/extensions/paragraph-spacing.js
@@ -9,7 +9,7 @@ import { __ } from '@wordpress/i18n';
 
 const MARGIN_ATTRIBUTE_NAME = 'customMargin';
 const PADDING_ATTRIBUTE_NAME = 'customPadding';
-const DISABLE_PADDING_ATTRIBUTE_NAME = 'disablePadding';
+const DISABLE_SPACING_ATTRIBUTE_NAME = 'disableSpacing';
 
 // Zoznam blokov, pre ktoré chceme pridať odsadenie
 const SUPPORTED_BLOCKS = ['core/paragraph', 'core/columns'];
@@ -31,7 +31,7 @@ function addSpacingAttributes(settings, name) {
 	};
 
 	if (name === 'core/columns') {
-		newAttributes[DISABLE_PADDING_ATTRIBUTE_NAME] = {
+		newAttributes[DISABLE_SPACING_ATTRIBUTE_NAME] = {
 			type: 'boolean',
 			default: false,
 		};
@@ -64,26 +64,31 @@ const withSpacingControl = createHigherOrderComponent((BlockEdit) => {
 				<BlockEdit {...props} />
 				<InspectorControls>
 					<PanelBody title={__('Odsadenie', 'slider')} initialOpen={false}>
-						<BoxControl
-							label={__('Margin', 'slider')}
-							values={attributes[MARGIN_ATTRIBUTE_NAME]}
-							onChange={(value) => setAttributes({ [MARGIN_ATTRIBUTE_NAME]: value })}
-							sides={['top', 'right', 'bottom', 'left']}
-							units={[{ value: 'px', label: 'px' }, { value: '%', label: '%' }, { value: 'em', label: 'em' }, {value: 'rem', label: 'rem'}, {value: 'vw', label: 'vw'}, {value: 'vh', label: 'vh'}]}
-							allowReset
-						/>
-						{name === 'core/columns' ? (
+						{name === 'core/columns' && (
 							<ToggleControl
-								label={__('Vypnúť paddingy', 'slider')}
-								checked={!!attributes[DISABLE_PADDING_ATTRIBUTE_NAME]}
-								onChange={(value) => setAttributes({ [DISABLE_PADDING_ATTRIBUTE_NAME]: value })}
+								label={__('Vypnúť marginy a paddingy', 'slider')}
+								checked={!!attributes[DISABLE_SPACING_ATTRIBUTE_NAME]}
+								onChange={(value) => setAttributes({ [DISABLE_SPACING_ATTRIBUTE_NAME]: value })}
 								help={
-									attributes[DISABLE_PADDING_ATTRIBUTE_NAME]
-										? __('Vlastné paddingy sú vypnuté (nastavené na 0).', 'slider')
-										: __('Zachovať pôvodné paddingy bloku.', 'slider')
+									attributes[DISABLE_SPACING_ATTRIBUTE_NAME]
+										? __('Vlastné marginy a paddingy sú vypnuté (nastavené na 0).', 'slider')
+										: __('Povoliť vlastné nastavenie pre marginy a paddingy.', 'slider')
 								}
 							/>
-						) : (
+						)}
+
+						{(!attributes[DISABLE_SPACING_ATTRIBUTE_NAME] || name !== 'core/columns') && (
+							<BoxControl
+								label={__('Margin', 'slider')}
+								values={attributes[MARGIN_ATTRIBUTE_NAME]}
+								onChange={(value) => setAttributes({ [MARGIN_ATTRIBUTE_NAME]: value })}
+								sides={['top', 'right', 'bottom', 'left']}
+								units={[{ value: 'px', label: 'px' }, { value: '%', label: '%' }, { value: 'em', label: 'em' }, {value: 'rem', label: 'rem'}, {value: 'vw', label: 'vw'}, {value: 'vh', label: 'vh'}]}
+								allowReset
+							/>
+						)}
+
+						{name !== 'core/columns' && (
 							<BoxControl
 								label={__('Padding', 'slider')}
 								values={attributes[PADDING_ATTRIBUTE_NAME]}
@@ -108,27 +113,33 @@ function applySpacingStylesToSave(extraProps, blockType, attributes) {
 		return extraProps;
 	}
 
-	const margin = attributes[MARGIN_ATTRIBUTE_NAME];
-	const padding = attributes[PADDING_ATTRIBUTE_NAME];
 	const newStyles = { ...extraProps.style };
 
-	if (margin) {
-		if (margin.top) newStyles.marginTop = margin.top;
-		if (margin.right) newStyles.marginRight = margin.right;
-		if (margin.bottom) newStyles.marginBottom = margin.bottom;
-		if (margin.left) newStyles.marginLeft = margin.left;
-	}
-
-	if (blockType.name === 'core/columns' && attributes[DISABLE_PADDING_ATTRIBUTE_NAME]) {
+	if (blockType.name === 'core/columns' && attributes[DISABLE_SPACING_ATTRIBUTE_NAME]) {
 		newStyles.paddingTop = '0px';
 		newStyles.paddingRight = '0px';
 		newStyles.paddingBottom = '0px';
 		newStyles.paddingLeft = '0px';
-	} else if (padding) {
-		if (padding.top) newStyles.paddingTop = padding.top;
-		if (padding.right) newStyles.paddingRight = padding.right;
-		if (padding.bottom) newStyles.paddingBottom = padding.bottom;
-		if (padding.left) newStyles.paddingLeft = padding.left;
+		newStyles.marginTop = '0px';
+		newStyles.marginRight = '0px';
+		newStyles.marginBottom = '0px';
+		newStyles.marginLeft = '0px';
+	} else {
+		const margin = attributes[MARGIN_ATTRIBUTE_NAME];
+		if (margin) {
+			if (margin.top) newStyles.marginTop = margin.top;
+			if (margin.right) newStyles.marginRight = margin.right;
+			if (margin.bottom) newStyles.marginBottom = margin.bottom;
+			if (margin.left) newStyles.marginLeft = margin.left;
+		}
+
+		const padding = attributes[PADDING_ATTRIBUTE_NAME];
+		if (padding) {
+			if (padding.top) newStyles.paddingTop = padding.top;
+			if (padding.right) newStyles.paddingRight = padding.right;
+			if (padding.bottom) newStyles.paddingBottom = padding.bottom;
+			if (padding.left) newStyles.paddingLeft = padding.left;
+		}
 	}
 
 	return { ...extraProps, style: newStyles };
